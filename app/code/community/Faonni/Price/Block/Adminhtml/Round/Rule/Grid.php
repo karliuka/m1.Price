f955d1b80a941498818299d5f6774189144419fd
karliuka <karliuka.vitalii@gmail.com>
2018-02-22 14:40:31 +0200

    grid

--- app/code/community/Faonni/Price/Block/Adminhtml/Round/Rule/Grid.php
+++ app/code/community/Faonni/Price/Block/Adminhtml/Round/Rule/Grid.php
@@ -1,5 +1,24 @@
 <?php
-{{license}}	
+/**
+ * Faonni
+ *  
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade module to newer
+ * versions in the future.
+ * 
+ * @package     Faonni_Price
+ * @copyright   Copyright (c) 2018 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class Faonni_Price_Block_Adminhtml_Round_Rule_Grid 
 	extends Mage_Adminhtml_Block_Widget_Grid
 {
